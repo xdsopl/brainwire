4a3a1e581584cff1c9c395e29bb3e435ead107b6
Ahmet Inan <xdsopl@gmail.com>
2024-09-02 17:44:55 +0200

    copy wav header as is

--- decode.c
+++ decode.c
@@ -22,9 +22,18 @@ int main(int argc, char **argv) {
 		fprintf(stderr, "could not open %s for reading\n", argv[1]);
 		return 1;
 	}
+	char wav_header[44];
+	if (fread(wav_header, sizeof(wav_header), 1, input) != 1) {
+		fprintf(stderr, "could not read 44 bytes from %s\n", argv[1]);
+		return 1;
+	}
 	FILE *output = fopen(argv[2], "w");
 	if (!output) {
 		fprintf(stderr, "could not open %s for writing\n", argv[2]);
+		return 1;
+	}
+	if (fwrite(wav_header, sizeof(wav_header), 1, output) != 1) {
+		fprintf(stderr, "could not write 44 bytes to %s\n", argv[2]);
 		return 1;
 	}
 	short value = 0;
